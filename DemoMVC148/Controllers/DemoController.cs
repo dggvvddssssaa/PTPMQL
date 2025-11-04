6dbeac87b67b7e0fee33e72009a712a5757167d6
dggvvddssssaa <dinhminhphuong677@gmail.com>
2025-11-04 10:09:03 +0700

    Tạo các action và view tương ứng

--- DemoMVC148/Controllers/DemoController.cs
+++ DemoMVC148/Controllers/DemoController.cs
@@ -1,31 +1,52 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using DemoMVC148.Models;
 
-namespace DemoMVC148.Controllers;
-
-public class DemoController : Controller
+namespace DemoMVC148.Controllers
 {
-    private readonly ILogger<DemoController> _logger;
-
-    public DemoController(ILogger<DemoController> logger)
+    public class DemoController : Controller
     {
-        _logger = logger;
-    }
+        // Action 1: ViewResult
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Buổi học số 3 - Ví dụ về các loại Result trong ASP.NET MVC";
+            return View();
+        }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        // Action 2: RedirectResult
+        public IActionResult GoToGoogle()
+        {
+            return Redirect("https://www.google.com");
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        // Action 3: RedirectToActionResult
+        public IActionResult RedirectToDemo()
+        {
+            return RedirectToAction("Index");
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        // Action 4: JsonResult
+        public IActionResult GetJson()
+        {
+            var data = new
+            {
+                Name = "Đinh minh Phương",
+                Subject = "ASP.NET MVC",
+                Lesson = 3
+            };
+            return Json(data);
+        }
+
+        // Action 5: FileResult
+        public IActionResult DownloadFile()
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "sample.txt");
+            var fileBytes = System.IO.File.ReadAllBytes(filePath);
+            return File(fileBytes, "text/plain", "sample.txt");
+        }
+
+        // Action 6: StatusCodeResult
+        public IActionResult ErrorPage()
+        {
+            return StatusCode(404, "Trang bạn yêu cầu không tồn tại!");
+        }
     }
 }
